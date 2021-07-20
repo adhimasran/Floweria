aef768e5a8b1622408a6223c30a36820edade744
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 <adhicittamasran@gmail.com>
2021-07-20 23:41:03 +0800

    Add view profile and profile action bar button

--- app/src/main/java/com/example/floweria/MainActivity.kt
+++ app/src/main/java/com/example/floweria/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.floweria
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -25,4 +28,21 @@ class MainActivity : AppCompatActivity() {
         val cardViewFlower = CardViewFlower(list, this)
         rvFlowers.adapter = cardViewFlower
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_action_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, ViewProfile::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
 }
