6fd986a20b3eca3e6f49ebb6d92ebb2194818f40
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 <adhicittamasran@gmail.com>
2021-07-20 22:47:29 +0800

    Add view profile

---
+++ app/src/main/java/com/example/floweria/ViewProfile.kt
@@ -0,0 +1,11 @@
+package com.example.floweria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_profile)
+    }
+}
