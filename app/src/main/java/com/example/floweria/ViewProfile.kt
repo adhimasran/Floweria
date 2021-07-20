fef0a4ffcf8f334a0c85ffc56a4f4c0f93c98e78
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 <adhicittamasran@gmail.com>
2021-07-20 23:57:53 +0800

    Add back button and set profile title action bar

--- app/src/main/java/com/example/floweria/ViewProfile.kt
+++ app/src/main/java/com/example/floweria/ViewProfile.kt
@@ -2,10 +2,16 @@ package com.example.floweria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class ViewProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_profile)
+
+        val actionBar : ActionBar? = supportActionBar
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+        actionBar.title = "Profile"
     }
 }
