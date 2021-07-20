8c4a842076771dca0380ba03b472e5b88b815aab
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 <adhicittamasran@gmail.com>
2021-07-20 20:26:51 +0800

    Add RecyclerView

--- app/src/main/java/com/example/floweria/MainActivity.kt
+++ app/src/main/java/com/example/floweria/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.floweria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvFlowers: RecyclerView
+    private var list: ArrayList<Flower> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvFlowers = findViewById(R.id.rv_flowers)
+        rvFlowers.setHasFixedSize(true)
+
+        list.addAll(FlowersData.listData)
+        showRecyclerCardView()
+    }
+
+    private fun showRecyclerCardView() {
+        rvFlowers.layoutManager = LinearLayoutManager(this)
+        val cardViewFlower = CardViewFlower(list)
+        rvFlowers.adapter = cardViewFlower
     }
 }
